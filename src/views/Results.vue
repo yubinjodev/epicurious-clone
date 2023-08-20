0f68a0dc727eb10b2103b324c5c4150ab50f5703
Yubin <yubinjodev@gmail.com>
2023-08-21 00:03:18 +0900

    feat: results

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,10 +1,22 @@
 <script setup>
+import $ from "jquery"
+import {ref} from "vue"
+import { useRoute } from "vue-router";
 import ResultCard from '../cards/ResultCard.vue';
+
+const route = useRoute()
+const query = route.query.search
+const results = ref({})
+
+$.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`, function(data){
+    results.value = data
+    console.log(results.value.meals.length);
+})
 </script>
 
 <template>
     <section class="Results-root">
-        <p class="caption">2,504 matching results for {{ $route.query.search }}</p>
+        <p class="caption">{{  results.meals.length }} matching results for {{ $route.query.search }}</p>
         <ResultCard/>
     </section>
 
